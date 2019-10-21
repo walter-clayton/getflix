e36cabef88dad66f8be080140e99afeec72f1006
Walter Clayton <walthedude@gmail.com>
2019-10-21 10:56:41 +0200

    made the basic layout of pages

---
+++ landing.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>landing page</title>
+</head>
+
+
+<body>
+<!-- HEADER -->
+<?php include("header.php"); ?>
+
+
+ <!--------FOOTER------->
+<?php include("footer.php"); ?>
+</body>
+
+
+</html>
