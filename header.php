44134a438edc7307fb9d2f254b6f56acc3120c7c
Walter Clayton <walthedude@gmail.com>
2019-10-23 12:12:16 +0200

    member page

--- header.php
+++ header.php
@@ -6,12 +6,12 @@
 <header>
 	<!-- NAVBAR -->
 	<nav class=" navbar navbar-expand-lg navbar-dark bg-black ">
-			<a class="navbar-brand hover" href="#"><img src="./images/fav.png" width="35" height="35" class="d-inline-block align-top hover" alt=""><span class="hover">   AFITPILOT</span> </a>
+			<a class="navbar-brand hover" href="#"><img src="./images/fav.png" width="35" height="35" class="d-inline-block align-top hover" alt=""><span class="hover">   GETFLIX</span> </a>
 			<button class="navbar-toggler"  type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" >
 		    <span class="navbar-toggler-icon"></span>
 		  	</button>
-			  <div class="collapse navbar-collapse" id="navbarText">
-				<ul class="navbar-nav">
+			  <div class="collapse navbar-collapse				
+			  <ul class= "navbar-nav" >
 				  <li class="nav-item">
 				    <a class="nav-link" href="index.php"><span class="hover">HOME</span></a>
 				  </li>
