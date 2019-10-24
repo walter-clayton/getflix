90c18984ed0059524a2fb6e28d526fd2053a488f
Walter Clayton <walthedude@gmail.com>
2019-10-24 10:50:51 +0200

    made color theme and member page with sign up

--- member.php
+++ member.php
@@ -1,47 +1,45 @@
 <?php
  
 
-$pseudo = "";
-$email = "";
-$password = "";
-$password_confirm = "";
+$pseudo = '';
+$password = '';
+$password_confirm = '';
 
 
 // isset to check whether the variables $pseudo and $message contain anything, it will return FALSE if the value is NULL
 if(isset($_POST['submit'])){ 
-    $pseudo = htmlspecialchars($_POST['pseudo']);
-    $email = htmlspecialchars($_POST['email']);
-    $pass_hashe = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $pseudo = htmlspecialchars($_POST['pseudo']); 
     $date_subscribed = "";
-    $_SESSION[""] = $pseudo;
+    $_SESSION[''] = $pseudo;
+
+    if ($_POST['password'] === $_POST['password_confirm']) {
+
+      $pass_hashe = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    }
+
+    else {
+      echo 'the passwords need to match!';
+    }
+
   }
 
 
 ?>
-
 <form class="form-horizontal" action='' method="POST">
   <fieldset>
+
     <div id="legend">
       <legend class="">Register</legend>
     </div>
     <div class="control-group">
       <!-- Username -->
-      <label class="control-label"  for="username">Pseudo</label>
+      <label class="control-label"  for="pseudo">Pseudo</label>
       <div class="controls">
         <input type="text" id="pseudo" name="pseudo" placeholder="" class="input-xlarge">
         <p class="help-block">Username can contain any letters or numbers, without spaces</p>
       </div>
     </div>
- 
-    <div class="control-group">
-      <!-- E-mail -->
-      <label class="control-label" for="email">E-mail</label>
-      <div class="controls">
-        <input type="text" id="email" name="email" placeholder="" class="input-xlarge">
-        <p class="help-block">Please provide your E-mail</p>
-      </div>
-    </div>
- 
     <div class="control-group">
       <!-- Password-->
       <label class="control-label" for="password">Password</label>
@@ -73,8 +71,6 @@ if(isset($_POST['submit'])){
 
 <?php
 
-if ( $password_confirm === $password )
-{
 
 
 
@@ -91,15 +87,21 @@ catch (Exception $e) {
 
 
 // insert the input into the database
-$req = $db->prepare('INSERT INTO members(pseudo, password, date_subscribed) VALUES (?, ?, curdate())');
+$req = $db->prepare('INSERT INTO members(pseudo, password, date_subscribed) VALUES (:pseudo, :pass_hashe, curdate())');
 $req->execute(array(
-  $pseudo, 
-  $password
+'pseudo' => $pseudo, 
+'pass_hashe' => $pass_hashe
   ));
-
 
 // get all data from minichat table, and most recent at the top, and set a limit of 10 lines
 $response = $db->query('SELECT * FROM members ORDER BY ID DESC LIMIT 0,1');
+
+$isPasswordCorrect = password_verify(password, $pass_hashe);
+
+if (isset($_SESSION['ID']) AND isset($_SESSION['pseudo'])) {
+
+  echo 'Bonjour' . $_SESSION['pseudo'];
+}
 
 // get the data and display it on the page
 while ($db = $response->fetch()){
@@ -109,9 +111,26 @@ while ($db = $response->fetch()){
 // frees up the connection to the server so that other SQL statements may be issued, but leaves the statement in a state that enables it to be executed again.
   $response->closeCursor();
 
-}
 
-else {
-  echo "Wrong password";
-}
-  ?>
+
+// if ( $isPasswordCorrect = FALSE ) {
+
+//   echo 'Wrong password';
+
+// }
+
+// else {
+
+//   if ($isPasswordCorrect) {
+//     session_start();
+//     $_SESSION['ID'] = $response['ID'];
+//     $_SESSION['pseudo'] = $pseudo;
+//     echo 'you are connected!';
+//   }
+//     else {
+//       echo 'Wrong password!';
+//     }
+
+//   }
+
+//   ?>
